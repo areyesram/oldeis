3b9d88f9fd51cf1c907417c98ee3c5d466a56c44
areyram <alejandro.reyes@abaton.com.mx>
2020-08-29 00:00:21 -0500

    mi

---
+++ mi/MainForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace areyesram
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(35F, 75F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Consolas", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(18, 17, 18, 17);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ mi/MainForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace areyesram
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _bounds = ClientRectangle;
+            _bounds.Inflate(-(_bounds.Width / 4), -(_bounds.Height / 4));
+            timer.Start();
+        }
+
+        private State _state = State.CountDown;
+        private int _count = 6;
+        private List<PointF> _pixels;
+        private List<PointF> _speed;
+        private Rectangle _bounds;
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            var w = ClientRectangle.Width;
+            var h = ClientRectangle.Height;
+            var r = new Random();
+            Bitmap bmp;
+            if (_state == State.CountDown)
+            {
+                _count--;
+                g.DrawString($"Este mensaje se autodestruirá en {_count} segundos.",
+                   Font, Brushes.White, _bounds);
+                if (_count == 0)
+                {
+                    bmp = new Bitmap(w, h);
+                    DrawToBitmap(bmp, ClientRectangle);
+                    _pixels = new List<PointF>();
+                    var back = bmp.GetPixel(0, 0);
+                    for (var x = 0; x < bmp.Width; x++)
+                    {
+                        for (var y = 0; y < bmp.Height; y++)
+                        {
+                            if (!bmp.GetPixel(x, y).Equals(back))
+                                _pixels.Add(new Point(x, y));
+                        }
+                    }
+                    _speed = _pixels.Select(p => new PointF(
+                            (float)r.NextDouble() * 40 - 20,
+                            (float)r.NextDouble() * 40 - 20))
+                        .ToList();
+                    _state = State.Explode;
+                    timer.Interval = 15;
+                }
+            }
+            else
+            {
+                _pixels = _pixels.Select((p, i) => new PointF(p.X + _speed[i].X, p.Y + _speed[i].Y)).ToList();
+                bmp = new Bitmap(w, h);
+                var f = false;
+                foreach (var p in _pixels)
+                {
+                    if (p.X > 0 && p.X < bmp.Width && p.Y > 0 && p.Y < bmp.Height)
+                    {
+                        bmp.SetPixel((int)p.X, (int)p.Y, Color.White);
+                        f = true;
+                    }
+                }
+                if (!f) Application.Exit();
+                g.DrawImageUnscaled(bmp, ClientRectangle);
+                _speed = _speed.Select(p => new PointF(p.X, p.Y + 0.5f)).ToList();
+            }
+        }
+
+        private enum State
+        {
+            CountDown,
+            Explode
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+    }
+}
